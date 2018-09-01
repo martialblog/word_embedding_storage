ce1703dc7992514cc276aad003c174d8d2b29f6f
Markus Opolka <markus@martialblog.de>
2018-09-01 22:01:23 +0200

    Update SQLite Example
     - Some minor renaming

--- examples/sqlite_example.py
+++ examples/sqlite_example.py
@@ -4,25 +4,34 @@
 import dummy
 import sqlite3
 import numpy
-import io
+from io import BytesIO
 
 
 def adapt_array(array):
     """
     Using the numpy.save function to save a binary version of the array,
     and BytesIO to catch the stream of data and convert it into a sqlite3.Binary.
+
+    :param numpy.array array: NumPy array to turn into sqlite3.Binary
+    :return: NumPy array as sqlite3.Binary
+    :rtype: sqlite3.Binary
     """
-    out = io.BytesIO()
+    out = BytesIO()
     numpy.save(out, array)
     out.seek(0)
 
     return sqlite3.Binary(out.read())
 
-def convert_array(text):
+
+def convert_array(blob):
     """
     Using BytesIO to convert the binary version of the array back into a numpy array.
+
+    :param sqlite3.Binary blob: sqlite3.Binary containing a NumPy array
+    :return: One steaming hot NumPy array
+    :rtype: numpy.array
     """
-    out = io.BytesIO(text)
+    out = BytesIO(blob)
     out.seek(0)
 
     return numpy.load(out)
@@ -49,3 +58,5 @@ for key, emb in dummy.embeddings():
 for key, emb in dummy.embeddings():
     cursor.execute('SELECT * FROM embeddings WHERE key=?', (key,))
     data = cursor.fetchone()
+
+connection.close()
